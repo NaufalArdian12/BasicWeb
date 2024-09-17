a60eadcc5e12016e9dd69c9f30127fb69d21e942
NaufalArdian12 <naufalopo87@gmail.com>
2024-09-17 08:58:46 +0700

    JS04 - Praktikum 1

---
+++ BasicWeb/Basic PHP (week4)/variabelkonstanta.php
@@ -0,0 +1,11 @@
+<?php
+$angka1 = 10;
+$angka2 = 5;
+$hasil = $angka1 + $angka2;
+echo "Hasil Penjumlahan $angka1 dan $angka2 adalah $hasil.";
+// Mendefinisiakan Konstanta untuk nilai tetap
+define("NAMA_SITUS", "WebsiteKu.com");
+define("Tahun_Pendirian", 2023);
+
+echo "Selamat datang di " . NAMA_SITUS . ", situs yang 
+didirikann pada tahun " . Tahun_Pendirian . ".";
