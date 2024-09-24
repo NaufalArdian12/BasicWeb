830d1146cf22d86dcc7f80c39da7bbf0ecda104b
NaufalArdian12 <naufalopo87@gmail.com>
2024-09-24 10:26:34 +0700

    JS05 - Pratikum 1

---
+++ BasicWeb/Basic PHP Part2 (week5)/array_2.php
@@ -0,0 +1,18 @@
+<?php
+$Dosen = [
+    'name' => 'Elok Nur Hamdana',
+    'city' => 'Malang',
+    'gender' => 'Perempuan',
+    'Address' =>'Jl. Kampung Melaku'
+];
+
+// Displaying array using keys
+// echo "Name: " . $Dosen['name'] . "<br>";
+// echo "City: " . $Dosen['city'] . "<br>";
+// echo "gender: " . $Dosen['gender'] . "<br>";
+
+echo "<table border='1'>";
+foreach ($Dosen as $key => $value) {
+    echo "<tr><td>$key</td><td>$value</td></tr>";
+}
+?>
