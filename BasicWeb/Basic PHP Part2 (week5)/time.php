27d159589cfb62a37d1d3c93691c24999991f4bb
NaufalArdian12 <naufalopo87@gmail.com>
2024-09-24 10:33:52 +0700

    JS05 - Pratikum 10

---
+++ BasicWeb/Basic PHP Part2 (week5)/time.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+<head></head>
+<body>
+  <h3>Time</h3>
+
+  <?php
+  date_default_timezone_set("Asia/Jakarta");  // Set the timezone to Asia/Jakarta (Indonesia's time zone)
+  echo date("h:i:sa");  // Display the current time in "hour:minute:second am/pm" format
+  ?>
+  
+</body>
+</html>
