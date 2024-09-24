5b74f58fdb54461796aeb258acf766844b6171dc
NaufalArdian12 <naufalopo87@gmail.com>
2024-09-24 10:33:26 +0700

    JS05 - Pratikum 9

---
+++ BasicWeb/Basic PHP Part2 (week5)/entities.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Entities HTML</title>
+</head>
+<body>
+  <p>It&#39;s time to read a HTML5 book.</p> 
+  <!-- &#39; is the HTML entity for an apostrophe (') -->
+  
+  <p>&nbsp;&nbsp;&nbsp;&nbsp;Keuntungan dari menggunakan nama entities: Sebuah nama entitas mudah diingat. Kerugian dari menggunakan nama entities: Browser mungkin tidak mendukung semua nama entitas, tetapi dukungan untuk nomor lebih baik.</p> 
+  <!-- &nbsp; is the HTML entity for a non-breaking space -->
+  
+  <p>&#169; 2024 jti.com</p>
+  <!-- &#169; is the HTML entity for the copyright symbol (©) -->
+</body>
+</html>
