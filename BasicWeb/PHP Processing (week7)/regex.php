12348bf0ffba5855f38ec09929d0cfed709246cc
NaufalArdian12 <naufalopo87@gmail.com>
2024-10-08 10:58:01 +0700

    JS07 - Praktikum 5

---
+++ BasicWeb/PHP Processing (week7)/regex.php
@@ -0,0 +1,30 @@
+<?php
+// $pattern = '/[a-z]/'; //cocokan huruf kecil.
+// $text = 'this is a sample text.';
+// if (preg_match($pattern, $text)) {
+//   echo "Huruf kecil ditemukan!";
+// } else {
+//   echo "Tidak ada huruf kecil!";
+// }
+
+// $pattern = '/[0-9]+/';
+// $text = 'there are 123 apples .';
+// if (preg_match($pattern, $text, $matches)){
+//   echo "cocokan: " . $matches[0];
+// } else {
+//   echo "Tidak ada yang cocok!";
+// }
+
+// $pattern = '/apple/';
+// $replacement = 'banana';
+// $text = 'I Like apple pie.';
+// $new_text = preg_replace($pattern, $replacement, $text);
+// echo $new_text;
+
+$pattern = '/n,m/';
+$text = 'god is good.';
+if (preg_match($pattern, $text, $matches)) {
+  echo "Cocokan: " . $matches[0];
+} else {
+  echo "Tidak ada yang cocok!";
+}
