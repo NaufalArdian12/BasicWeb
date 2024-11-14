c50e084f3acd4bf5c811809619baa4b2bc183a7c
NaufalArdian12 <naufalopo87@gmail.com>
2024-11-14 15:48:37 +0700

    templates

---
+++ BasicWeb/PHP Modular/admin/templatess/footer.php
@@ -0,0 +1,17 @@
+<!-- Load Bootstrap JavaScript Bundle -->
+<script src="assets\js\bootstrap.bundle.min.js"></script>
+<script>
+ 
+  src="https://cdn.jsdelivr.net/npm/chart.js"
+  integrity="sha384-eI7PSr3L1xLlISH8JdDII5YN/njoSsxfbrkCnJnrzXt+ENP5MOVBxD+Lb6EG4zopL"
+  crossorigin="anonymous"
+</script>
+<!-- Load Chart.js from CDN -->
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+
+<!-- Load Custom Dashboard Scripts -->
+<script src="assets\js\dashboard.js"></script>
+</body>
+
+</html>
